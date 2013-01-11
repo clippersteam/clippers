86073706c0181f427d8267aec897ef2a1a3e4077
yeaz <yeaz@stanford.edu>
2013-01-11 20:56:27 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clippers::Application.config.secret_token = '7666a7483006a4505664842a753e67bf9c3a3b2bebfa91126a2c286992c085bfcf3c1a574775e9c1781f915fa65902bfda9bf7bdf64704527b16eab6e2292f56'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Clippers::Application.routes.draw do
+  root to: 'home#index'
+end
